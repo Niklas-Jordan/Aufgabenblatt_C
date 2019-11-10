c8615b138886cbc8b75bb9cbcb82d749d0465c9c
Niklas <Niklas.Jordan@hochschule-stralsund.de>
2019-11-10 03:09:24 +0100

    C7 beendet, nur noch auf Richtigkeit prüfen/testen C8 siehe Vorlesung PTII, Semester II

--- src/LPS_Niklas_Jordan_SMIB/Zettelkasten.java
+++ src/LPS_Niklas_Jordan_SMIB/Zettelkasten.java
@@ -14,17 +14,56 @@ public class Zettelkasten implements Iterable<Medium> {
 
     boolean addMedium(Medium data) {
         myZettelkasten.add(data);
+        order = null;
         return true;
     }
 
-    public boolean dropMedium(String titel) throws DuplicateEntryException {
-            for (Medium data : myZettelkasten) {
-               if (data.getTitel().equals(titel)) {
-                   throw new DuplicateEntryException("Eingegebener Titel ist nur einmal vorhanden!");
-               }
-           }
-         return myZettelkasten.remove(findSoloMedium(titel));
+    public void dropMedium(String titel)throws DuplicateEntryException {
+        ArrayList<Medium> copies = new ArrayList<>();
+        int data = 0;
+        for (Medium test : myZettelkasten) {
+
+            if (test.getTitel() == titel) {
+                copies.add(test);
+                data++;
+                if(copies.size()>1){
+                    throw new DuplicateEntryException("DuplicateEntryExeption\n");
+                }
+            }else if(!myZettelkasten.contains(findSoloMedium(titel))) {
+                System.out.println("Titel existiert nicht!\n");
+                break;
+            }else{
+            }
         }
+        if(data > 0 && data < 2){
+            myZettelkasten.remove(copies.get(0));
+        }
+    }
+
+    public boolean dropMedium(String titel, int index) throws DuplicateEntryException {
+        ArrayList<Medium> copies = new ArrayList<>();
+        for(Medium data : myZettelkasten) {
+            if(data.getTitel() == titel) {
+                copies.add(data);
+            }
+        }
+        myZettelkasten.remove(copies.get(index));
+        System.out.println(copies);
+        return true;
+    }
+
+    public boolean dropMedium(String titel, String alleLöschen) {
+        if(alleLöschen == "ja") {
+            boolean gelöscht = false;
+            for (Medium data : findMedium(titel)) {
+                gelöscht |= myZettelkasten.remove(data);
+            }
+            return gelöscht;
+        } else {
+            System.out.println("Geben sie 'ja' ein, um alle Duplikate zu löschen!");
+        }
+        return true;
+    }
 
     private Boolean order = null;
 
@@ -54,12 +93,35 @@ public class Zettelkasten implements Iterable<Medium> {
     }
 
     public List<Medium> findMedium(String titel) {
+        int i = 0;
         List<Medium> copies = new ArrayList<>();
-        for (Medium data : myZettelkasten) {
-            if (data.getTitel().equals(titel)) {
+        List<Medium> copiesSorted = new ArrayList<>();
+        List<Medium> Zeitschrift = new ArrayList<>();
+        List<Medium> CD = new ArrayList<>();
+        List<Medium> Buch = new ArrayList<>();
+        List<Medium> ElektronischesMedium = new ArrayList<>();
+
+        for(Medium data : myZettelkasten) {
+            if(data.getTitel().equals(titel)) {
                 copies.add(data);
+                if(data.getClass().getName() == "Zeitschrift") {
+                    Zeitschrift.add(data);
+                }else if(data.getClass().getName() == "com.company.CD") {
+                    CD.add(data);
+                }else if(data.getClass().getName() == "com.company.Buch") {
+                    Buch.add(data);
+                }else if(data.getClass().getName() == "com.company.ElektronischesMedium") {
+                    ElektronischesMedium.add(data);
+                } else {
+                    i++;
+                }
             }
         }
-        return copies;
+        copiesSorted.addAll(Zeitschrift);
+        copiesSorted.addAll(CD);
+        copiesSorted.addAll(Buch);
+        copiesSorted.addAll(ElektronischesMedium);
+
+        return copiesSorted;
     }
 }
